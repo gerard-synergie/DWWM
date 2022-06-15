d3f7a131dae27b3fbd07773ffb99d5ebff5be931
Gerard <gerard.tourres@gmail.com>
2022-06-15 18:58:38 +0200

    [FIX] index urlCreator

--- views/invoice/index.php
+++ views/invoice/index.php
@@ -1,9 +1,45 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>invoice/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\InvoiceSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Invoices';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="invoice-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Invoice', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id_invoice',
+            'date',
+            'summary',
+            'fk_customer',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, \app\models\Invoice $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id_invoice' => $model->id_invoice]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
